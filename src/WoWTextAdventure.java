a92d288fe9bdfc65c269079e92554a43addefe55
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2024-11-05 10:04:33 +0100

    Die Standard wege können benutzt werden

--- src/WoWTextAdventure.java
+++ src/WoWTextAdventure.java
@@ -7,17 +7,36 @@ public class WoWTextAdventure {
         System.out.println("Verwende 'N' , 'O', 's' oder 'W', um dich zu bewegen mit 'I' rufst du das Iventar auf.");
         System.out.println("Mit 'Q' beendest du das Spiel");
         Scanner scanner = new Scanner(System.in);
-
-       String eingabe = scanner.nextLine();
-
-        switch (eingabe.toLowerCase()){
-            case "n" -> {
-                System.out.println("hello");
+        String[] iventar = null;
+        boolean gameScheleife = true;
+        while (gameScheleife) {
+            String eingabe = scanner.nextLine();
+            switch (eingabe.toLowerCase()) {
+                case "n" -> {
+                    System.out.println("Du geht nach Norden");
+                }
+                case "o" -> {
+                    System.out.println("Du gehst nach Osten");
+                }
+                case "s" -> {
+                    System.out.println("Du geht nach Süden");
+                }
+                case "w" -> {
+                    System.out.println("Du geht nach Westen");
+                }
+                case "i" -> {
+                    System.out.println("In deinem Iventar befinde sich:");
+                    if (iventar == null) {
+                        System.out.println("Du hast keine Items");
+                    } else {
+                        System.out.println("Du hast diese items:");
+                    }
+                }
+                case "q" -> {
+                    System.out.println("Auf Wiedersehen!");
+                    gameScheleife = false;
+                }
             }
-            case "d" -> {
-                System.out.println("be");
-            }
-
 
         }
     }
