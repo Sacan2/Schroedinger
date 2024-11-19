d1d68efb94b2467fae92fdf12f0c3cc1dd91ce5f
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2024-11-19 19:43:34 +0100

    Den vierten Test überprüfen

--- src/Wertebereich.java
+++ src/Wertebereich.java
@@ -5,6 +5,7 @@ public class Wertebereich {
         int ersterTest = 1;
         Long zweiterTest = 502220L;
         float dritterTest = 3.002221F;
+        int vierterTest = 5224;
 
 
 
@@ -12,7 +13,8 @@ public class Wertebereich {
 
         System.out.println("Der erste Wert passt in Long? : "+ (ersterTest >= Long.MIN_VALUE && ersterTest <= Long.MAX_VALUE));
         System.out.println("Der zweite Wert passt in short? : "+ (zweiterTest >= Short.MIN_VALUE && zweiterTest <= Short.MAX_VALUE));
-        System.out.println("Der deitte Wert passt in double? : "+ (dritterTest >= Double.MIN_VALUE && dritterTest <= Double.MAX_VALUE));
+        System.out.println("Der dritte Wert passt in double? : "+ (dritterTest >= Double.MIN_VALUE && dritterTest <= Double.MAX_VALUE));
+        System.out.println("Der vierte Wert passt in byte? : "+ (vierterTest >= Byte.MIN_VALUE && vierterTest <= Byte.MAX_VALUE));
 
 
         Typetester typetester = new Typetester();
