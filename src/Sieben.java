d260dc9b758f4cb70ed92f12577c25014c40539c
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2024-11-20 22:14:36 +0100

    Kleine abänderung

--- src/Sieben.java
+++ src/Sieben.java
@@ -4,7 +4,7 @@ public class Sieben {
         int zahl = 123;
 
         if (zahl%7 == 0){
-            System.out.println("Ist Teilbar durch 7");
+            System.out.println(zahl + " ist Teilbar durch 7");
         }
         else{
             System.out.println("Ist nicht Teilbar durch 7");
