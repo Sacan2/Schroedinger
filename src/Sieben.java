8364e114956ca6b8b9fea955b1f35c5bf08e4823
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2024-11-20 22:19:46 +0100

    Weitere Aufgabe prüfen, ob der Parameter auch durch 13 Teilbar ist

--- src/Sieben.java
+++ src/Sieben.java
@@ -1,10 +1,10 @@
 public class Sieben {
     public static void main(String[] args) {
 
-        int zahl = 123;
+        int zahl = 26;
 
-        if (zahl%7 == 0){
-            System.out.println(zahl + " ist Teilbar durch 7");
+        if (zahl%7 == 0 || zahl%13 == 0){
+            System.out.println(zahl + " ist Teilbar durch 7 oder du hattest Glück");
         }
         else{
             System.out.println(zahl + " ist nicht Teilbar durch 7");
