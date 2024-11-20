b816b5abce2e52c8b6bad0c107149028aa13c015
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2024-11-20 22:13:58 +0100

    Aufgabe beendet

--- src/Sieben.java
+++ src/Sieben.java
@@ -1,2 +1,14 @@
 public class Sieben {
+    public static void main(String[] args) {
+
+        int zahl = 123;
+
+        if (zahl%7 == 0){
+            System.out.println("Ist Teilbar durch 7");
+        }
+        else{
+            System.out.println("Ist nicht Teilbar durch 7");
+        }
+
+    }
 }
