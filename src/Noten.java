f2c19dd854093179ee079050527d54ffe5bf2caf
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2024-11-21 23:13:03 +0100

    Kapitel 3 Schulnoten: Aufgabe beendet

--- src/Noten.java
+++ src/Noten.java
@@ -1,2 +1,30 @@
 public class Noten {
+    public static void main(String[] args) {
+        int note = 4;
+
+        switch(note){
+            case 1:
+                System.out.println("Sehr gut");
+                break;
+            case 2:
+                System.out.println("Gut");
+                break;
+            case 3:
+                System.out.println("Befriedigend");
+                break;
+            case 4:
+                System.out.println("Ausreichend");
+                break;
+            case 5:
+                System.out.println("Mangelhaft");
+                break;
+            case 6:
+                System.out.println("Ungenügend");
+                break;
+            default:
+                System.out.println("Mach eine Zahl von 1 bis 6 IDIOT!!!");
+        }
+
+
+    }
 }
