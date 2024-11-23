97ccfb4fdbb8a5af7e3f6415c4d5667bcbffe08a
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2024-11-23 12:00:53 +0100

    Kapitel 3 Gegner Treffen: Aufgabe ein wenig besser gemacht

--- src/Gegner.java
+++ src/Gegner.java
@@ -3,24 +3,31 @@ import java.util.Scanner;
 public class Gegner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
 
+        boolean gameLoop = true;
 
-        switch (input) {
-            case 0:
-                System.out.println("Du hast den Gegner verfehlt!");
-                break;
-            case 1,2,3:
-                System.out.println("Du hast den Gegner Schwach getroffen!");
-                break;
-            case 4,5,6:
-                System.out.println("Du hast den Gegner Normal getroffen!");
-                break;
-            case 7,8,9,10:
-                System.out.println("Du hast einen KRITISCHEN Treffer gelandet!!!");
-                break;
-            default:
-                System.out.println("Du bist absolut schlecht");
+        while (gameLoop) {
+            int input = scanner.nextInt();
+            switch (input) {
+                case 0:
+                    System.out.println("Du hast den Gegner verfehlt!");
+                    gameLoop = false;
+                    break;
+                case 1, 2, 3:
+                    System.out.println("Du hast den Gegner Schwach getroffen!");
+                    gameLoop = false;
+                    break;
+                case 4, 5, 6:
+                    System.out.println("Du hast den Gegner Normal getroffen!");
+                    gameLoop = false;
+                    break;
+                case 7, 8, 9, 10:
+                    System.out.println("Du hast einen KRITISCHEN Treffer gelandet!!!");
+                    gameLoop = false;
+                    break;
+                default:
+                    System.out.println("Du bist absolut schlecht");
+            }
         }
     }
 }
