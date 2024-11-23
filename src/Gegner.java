0f97620015cd087dbf8799c5608827ea422f3ef3
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2024-11-23 11:51:25 +0100

    Kapitel 3 Gegner Treffen: Erstelle ein Programm, das ausgibt, wie stark du einen Gegner getroffen hast. 0 = Du hast ihn verpasst 1-3 = Schwach getroffen 4-7 = Normal getroffen 8-10 = Kritischer Treffer

---
+++ src/Gegner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Gegner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
+
+      /*  switch (treffer) {
+            case 0:
+                System.out.println("Du hast den Gegner verfehlt!");
+                break;*/
+        }
+    }
+}
