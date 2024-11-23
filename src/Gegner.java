666954cf1796265eec9460c5a4ad64591e162cab
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2024-11-23 11:58:44 +0100

    Kapitel 3 Gegner Treffen: Aufgabe beendet wie es gewollt ist

--- src/Gegner.java
+++ src/Gegner.java
@@ -3,13 +3,24 @@ import java.util.Scanner;
 public class Gegner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input);
+        int input = scanner.nextInt();
 
-      /*  switch (treffer) {
+
+        switch (input) {
             case 0:
                 System.out.println("Du hast den Gegner verfehlt!");
-                break;*/
+                break;
+            case 1,2,3:
+                System.out.println("Du hast den Gegner Schwach getroffen!");
+                break;
+            case 4,5,6:
+                System.out.println("Du hast den Gegner Normal getroffen!");
+                break;
+            case 7,8,9,10:
+                System.out.println("Du hast einen KRITISCHEN Treffer gelandet!!!");
+                break;
+            default:
+                System.out.println("Du bist absolut schlecht");
         }
     }
 }
