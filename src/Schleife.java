b6aa1ce26eaa8e5de610bf5d21979b60d54c5c2b
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2024-11-29 20:51:15 +0100

    Kapitel 3 Standard Schleifen:
    Primzahl Aufgabe

--- src/Schleife.java
+++ src/Schleife.java
@@ -1,8 +1,23 @@
 public class Schleife {
     public static void main(String[] args) {
 
-        for (int i = 10;i > 0; i--){
-            System.out.println(i);
+        for (int i = 100; i > 0; i--) {
+            boolean istPrimzahl = true;
+
+            // Überprüfe, ob i eine Primzahl ist
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    istPrimzahl = false;
+                    break;
+                }
+            }
+
+            // Ausgabe basierend auf der Primzahlprüfung
+            if (istPrimzahl && i > 1) {
+                System.out.println("Ist eine Primzahl: " + i);
+            } else {
+                System.out.println("Ist keine Primzahl: " + i);
+            }
         }
     }
 }
